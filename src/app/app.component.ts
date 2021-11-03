7644089f5bc415536f874022eb3e648aa9e7b6d2
madonnaredakamel <madonna.reda.25@gmail.com>
2021-11-03 15:50:12 +0200

    translate function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
+import { DOCUMENT } from "@angular/common";
+import { Inject } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'angular-localization';
+  title = "angular-localization";
+
+  constructor(
+    private translateService: TranslateService,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
+
+  changeLangage(lang: string) {
+    let htmlTag = this.document.getElementsByTagName(
+      "html"
+    )[0] as HTMLHtmlElement;
+    htmlTag.dir = lang === "ar" ? "rtl" : "ltr";
+    this.translateService.setDefaultLang(lang);
+    this.translateService.use(lang);
+    this.changeCssFile(lang);
+  }
+
+  changeCssFile(lang: string) {
+    let headTag = this.document.getElementsByTagName(
+      "head"
+    )[0] as HTMLHeadElement;
+    let existingLink = this.document.getElementById(
+      "langCss"
+    ) as HTMLLinkElement;
+
+    let bundleName = lang === "ar" ? "arabicStyle.css" : "englishStyle.css";
+
+    if (existingLink) {
+      existingLink.href = bundleName;
+    } else {
+      let newLink = this.document.createElement("link");
+      newLink.rel = "stylesheet";
+      newLink.type = "text/css";
+      newLink.id = "langCss";
+      newLink.href = bundleName;
+      headTag.appendChild(newLink);
+    }
+  }
 }
